f8abb38a997cdd5fa222f675c9f8c7d8d3822051
Tomoe1046 <nine6642@gmail.com>
2023-09-21 21:13:21 +0700

    Change size and printf of bloodgroup.c

--- bloodgroup.c
+++ bloodgroup.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 void main()
-{ char Name[100], G[5], answer[10];
+{ char Name[80], G[5], answer[10];
   int i;
   do
   {
-    printf("What is your Name? ");
+    printf("What is YOUR Name? ");
     scanf("%s", Name);
     do
     {
